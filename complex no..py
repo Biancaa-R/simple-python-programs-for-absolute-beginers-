79d1b6b46379d524f87e2cd0ce5b18e22e3c93fd
Biancaa.R <87770461+Biancaa-R@users.noreply.github.com>
2022-11-24 20:50:18 +0530

    Add files via upload

---
+++ complex no..py
@@ -0,0 +1,9 @@
+'''Write a program to read two complex numbers from the user and find their sum,
+difference and product.'''
+
+c1=complex(input("enter the first complex number"))
+c2=complex(input("enter the second complex number"))
+
+Sum=c1+c2
+prod=c1*c2
+print(Sum," is the sum of the complex numbers",prod,"is the product of the complex numbers")
