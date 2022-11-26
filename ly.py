fa8ab34842bddeaff5a85a148000a403c7408150
Biancaa.R <87770461+Biancaa-R@users.noreply.github.com>
2022-11-26 10:52:16 +0530

    Add files via upload

---
+++ ly.py
@@ -0,0 +1,8 @@
+'''Light travels at 3 * 10 8 meters per second. A light-year is the distance a light beam travels in one year. Write a program that calculates and displays the value of a lightyear. [Light year = speed of light * number of seconds in a year]'''
+
+print("to calculate the distance travelled by light in one year in meters")
+
+light_yr=3*(10**8)*24*60*60*365
+print(light_yr, "is the distance travelled by light in one year")
+
+
