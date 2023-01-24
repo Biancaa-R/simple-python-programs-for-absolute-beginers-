ff66768feea87166a248674657527ced3d41dfd6
Biancaa.R <87770461+Biancaa-R@users.noreply.github.com>
2023-01-24 12:25:42 +0530

    Add files via upload

---
+++ Assignment7/length.py
@@ -0,0 +1,3 @@
+'''5.Write a Python program to display the list of words which are greater than  the length “n” from a given list of words. Example : “Hello  Welcome to the world of python”If n=3o/p: [“Hello”,”welcome”,”world”,”Python”]'''
+
+
---
+++ Assignment7/zip.py
@@ -0,0 +1,6 @@
+'''4.Concatenate two lists index wise using zip function:Example: list1 = ["M", "na", "i", "Ke"]                 list2 = ["y", "me", "s", "lly"]          output: ['My', 'name', 'is', 'Kelly']'''
+
+list1 = ["M", "na", "i", "Ke"]                 
+list2 = ["y", "me", "s", "lly"] 
+l=zip(list1,list2)
+print(l)
